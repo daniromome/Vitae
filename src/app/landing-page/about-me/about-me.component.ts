9c2c00eab7916ceba2bce58d4ccf1f460892ad95
dani9oo <dani9oo.dev@gmail.com>
2020-11-28 01:39:47 -0600

    About me card creation

---
+++ src/app/landing-page/about-me/about-me.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-about-me',
+  templateUrl: './about-me.component.html',
+  styleUrls: ['./about-me.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AboutMeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
