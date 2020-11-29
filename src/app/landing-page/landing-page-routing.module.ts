8060ae2fe50ba6920a10d28b1ab5a66a361258ac
dani9oo <dani9oo.dev@gmail.com>
2020-11-28 19:38:28 -0600

    Added icon support for this module

--- src/app/landing-page/landing-page-routing.module.ts
+++ src/app/landing-page/landing-page-routing.module.ts
@@ -1,3 +1,4 @@
+import { MatIconModule } from '@angular/material/icon';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -7,6 +8,6 @@ const routes: Routes = [{ path: '', component: LandingPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule, MatIconModule]
 })
 export class LandingPageRoutingModule { }
