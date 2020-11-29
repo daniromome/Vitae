e39fb1c3b00415600e4f1d1036cd97fa76191352
dani9oo <dani9oo.dev@gmail.com>
2020-11-28 19:38:44 -0600

    Added card and button modules

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,18 +1,21 @@
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 
 @NgModule({
-  declarations: [LandingPageComponent, AboutMeComponent],
+  declarations: [LandingPageComponent, AboutMeComponent, SkillsComponent],
   imports: [
     CommonModule,
     LandingPageRoutingModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class LandingPageModule { }
