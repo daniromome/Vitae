21d82127f903c4c88a554614a2160204b090a8ac
dani9oo <dani9oo.dev@gmail.com>
2020-12-03 01:17:44 -0600

    Added services initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { PortfolioService } from './landing-page/portfolio/portfolio.service';
+import { SkillsService } from './landing-page/skills/skills.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Vitae';
+
+  constructor(
+    private skills: SkillsService,
+    private projects: PortfolioService
+  ) {}
+
+  ngOnInit(): void {
+    this.skills.init();
+    this.projects.init();
+  }
 }
