7e7fe93c7365401d4f42d249737d2b48d687c6ee
dani9oo <dani9oo.dev@gmail.com>
2020-12-03 01:18:56 -0600

    Added portfolio and contact components

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -8,10 +8,12 @@ import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
-  declarations: [LandingPageComponent, AboutMeComponent, SkillsComponent],
+  declarations: [LandingPageComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ContactComponent],
   imports: [
     CommonModule,
     LandingPageRoutingModule,
