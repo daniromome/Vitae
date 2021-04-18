19fc03d67d4d1c790f4c0fdc862e6cd8b1ba176f
dani9oo <dani9oo.dev@gmail.com>
2021-04-17 22:10:50 -0500

    Added reactive logic to dinamically calculate age

--- src/app/landing-page/about-me/about-me.component.ts
+++ src/app/landing-page/about-me/about-me.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { AboutMeService } from './about-me.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-about-me',
@@ -8,7 +10,25 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
-  constructor() { }
+  ageInSeconds$: Observable<Duration>;
+
+  constructor(private aboutMeService: AboutMeService) {
+    this.ageInSeconds$ = this.aboutMeService.getAge;
+  }
+
+  readableAge(d: Duration | null) {
+    if (!d) return '?';
+    const months = this.getProperAmountNouns(d.months, 'month');
+    const days = this.getProperAmountNouns(d.days, 'day');
+    const minutes = this.getProperAmountNouns(d.minutes, 'minute');
+    const hours = this.getProperAmountNouns(d.hours, 'hour');
+    const seconds = this.getProperAmountNouns(d.seconds, 'second');
+    return `${d.years} years, ${months} ${days} ${hours} ${minutes} and ${seconds} ago`;
+  }
+
+  getProperAmountNouns(value: number | undefined, singular: string) {
+    return value === 0 || value === undefined ? `0 ${singular}s` : value === 1 ? `${value} ${singular}` : `${value} ${singular}s`;
+  }
 
   ngOnInit(): void {
   }
